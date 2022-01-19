0a5613f4f87bec157d1c55df925181d3578f9487
jason.chou <jason.chou@ddhl.com>
2022-01-19 13:07:37 +0800

    add project

---
+++ cat_video.py
@@ -0,0 +1,165 @@
+import argparse
+import os
+from typing import List, Tuple
+
+
+name_tag = '0123456789abcdefghijklmnopqrstuvwxyz'
+
+
+def parse_args() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    for i in range(1, 6):
+        parser.add_argument(f'-i{i}', f'--input{i}', type=str, help='input video {i}')
+        parser.add_argument(f'-c{i}', f'--crop{i}', type=str, help='crop {i} usage: -c{i} w:h:x:y')
+        parser.add_argument(f'-s{i}', f'--scale{i}', type=str, help='scale {i} usage: -s{i} w:h')
+    parser.add_argument('-m', '--map', type=str, help='mapping')
+    parser.add_argument('-v', '--vertical', help='vertical mode', action='store_true')
+    parser.add_argument('-y', '--override', help='yes if override', action='store_true')
+    parser.add_argument('-o', '--output', type=str, help='output file name (default= output.mp4)', default='output.mp4')
+    return parser.parse_args()
+
+
+def parse_map_index(s: str):
+    res = []
+    if '+' in s:
+        plus_ls = s.split('+')
+        for e in plus_ls:
+            res.append(parse_map_index(e))
+        return res
+    if '*' in s:
+        mul_ls = s.split('*')
+        if len(mul_ls) != 2:
+            raise ValueError(f'parse mapping error: {s}')
+        res = {'v': int(mul_ls[0]) - 1, 'alpha': float(mul_ls[1])}
+    else:
+        res = {'v': int(s) - 1}
+    return res
+
+
+def parse_map(mapping: str):
+    res = []
+    for each in mapping.split(','):
+        res.append(parse_map_index(each))
+
+    return res
+
+
+def video_to_str(m: dict, args: argparse.ArgumentParser, tag_name: str, width: int = None, vertical: bool = False):
+    v = m['v']
+    mcmd = f'[{v}:v]setpts=PTS-STARTPTS'
+    v += 1
+    if args.__dict__[f'crop{v}'] is not None:
+        mcmd += ', crop=' + args.__dict__[f'crop{v}']
+    if args.__dict__[f'scale{v}'] is not None:
+        mcmd += ', scale=' + args.__dict__[f'scale{v}']
+    if 'alpha' in m:
+        alpha = m['alpha']
+        mcmd += f', format=yuva420p,colorchannelmixer=aa={alpha}'
+    if width is not None:
+        if vertical:
+            mcmd += f', pad=iw:ih*{width}'
+        else:
+            mcmd += f', pad=iw*{width}:ih'
+
+    mcmd += f'[{tag_name}];'
+
+    return mcmd
+
+
+def map_to_str(map_ls: List[dict], args: argparse.ArgumentParser, index: int) -> Tuple[str, str, int]:
+    global name_tag
+    mcmd = ''
+    last_tag = ''
+    for j in range(len(map_ls) - 1):
+        last_tag = name_tag[j]
+        mcmd += video_to_str(map_ls[j], args, last_tag)
+        if j + 1 != len(map_ls):
+            tag1 = last_tag
+            last_tag = name_tag[j + 1]
+            mcmd += video_to_str(map_ls[j + 1], args, last_tag)
+            tag2 = last_tag
+
+            last_tag = name_tag[index]
+            index += 1
+            mcmd += f'[{tag1}][{tag2}] overlay=shortest=1[{last_tag}];'
+
+    return mcmd, last_tag, index
+
+
+def main():
+    global name_tag
+    args = parse_args()
+    ls = []
+    for i in range(1, 6):
+        if args.__dict__[f'input{i}'] is not None:
+            ls.append(args.__dict__[f'input{i}'])
+
+    mapping = args.map
+
+    if mapping is None:
+        mapping = ''
+        for i in range(len(ls)):
+            f = ls[i]
+            if f is not None:
+                if i == 0:
+                    mapping += f'{i}'
+                else:
+                    mapping += f', {i}'
+
+    mapping = parse_map(mapping)
+
+    width = len(mapping)
+    cmd = 'ffmpeg '
+    if args.override:
+        cmd += '-y '
+    for i in range(len(ls)):
+        cmd += f'-i {ls[i]} '
+    cmd += '-filter_complex "'
+
+    index = width + 1
+
+    tags = {}
+    for i in range(len(mapping)):
+        m_ls = mapping[i]
+        if len(m_ls) > 1:
+            mcmd, tag, index = map_to_str(m_ls, args, index)
+            cmd += mcmd
+            tags[i] = tag
+
+    cmd += ''
+    for i in range(len(mapping)):
+        if i not in tags:
+            m_ls = mapping[i]
+            cmd += video_to_str(m_ls, args, name_tag[i], width if i == 0 else None, args.vertical)
+            tags[i] = name_tag[i]
+
+    last_tag = tags[0]
+
+    if args.vertical:
+        for i in range(len(mapping) - 1):
+            tag1 = last_tag
+            tag2 = tags[i + 1]
+
+            last_tag = name_tag[index]
+            index += 1
+            cmd += f'[{tag1}][{tag2}]overlay=0:H*{i + 1}/{width}[{last_tag}];'
+    else:
+        for i in range(len(mapping) - 1):
+            tag1 = last_tag
+            tag2 = tags[i + 1]
+
+            last_tag = name_tag[index]
+            index += 1
+            cmd += f'[{tag1}][{tag2}]overlay=W*{i + 1}/{width}:0[{last_tag}];'
+
+    cmd = cmd[:-1]
+    cmd += '"'
+
+    cmd += f' -map "[{last_tag}]" {args.output}'
+    print(f'========= cat_video run: {cmd}')
+    os.system(cmd)
+    print(f'========= cat_video done: {cmd}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ dumpper.py
@@ -0,0 +1,71 @@
+import argparse
+import os
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, help='input video file', required=True)
+    parser.add_argument('-ia', '--input_audio', type=str, help='input audio file')
+    parser.add_argument('-o', '--output', type=str, help='output folder (default is ./results/)', default='./results/')
+    parser.add_argument('--dump_image', help=(
+        'dump image mode usage: '
+        'python ffmpeg_helper.py -i video.mp4 -o dump/images/ --dump_image'
+    ), action='store_true')
+    parser.add_argument('--dump_audio', help=(
+        'dump audio mode usage: '
+        'python ffmpeg_helper.py -i video.mp4 -o dump/audio.aac --dump_audio'
+    ), action='store_true')
+    parser.add_argument('--dump_meta', help=(
+        'dump metadata mode usage: '
+        'python ffmpeg_helper.py -i video.mp4 -o dump/metadata.txt --dump_meta'
+    ), action='store_true')
+    parser.add_argument('-ra', '--replace_audio', help=(
+        'replace video audio from input audio file usage: '
+        'python ffmpeg_helper.py -i video.mp4 -o results/video.mp4 -ia audio.aac --replace_audio'
+    ), action='store_true')
+    parser.add_argument('--fps', type=int, help='fps rate (-1 to use default)', default=-1)
+    parser.add_argument('--dump_quality', type=int, help=(
+        'dump quality for jpg (normal range is 2-31 31 is worst)'), default=2)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    cmd = f'ffmpeg -i {args.input}'
+    if args.input_audio is not None:
+        cmd += f' -i {args.input_audio}'
+
+    fps = args.fps
+
+    output = args.output.lower()
+    if args.dump_image:
+        print('dump image...')
+        os.makedirs(output, exist_ok=True)
+        if not ('.png' in output or '.jpg' in output or '.jpeg' in output):
+            output = os.path.join(output, '%05d.jpg')
+        output = f' {output}'
+        if args.dump_quality > 0:
+            output = f' -q:v {args.dump_quality} {output}'
+        if fps > 0:
+            output = f' -vf "fps={fps}"{output}'
+    elif args.dump_audio:
+        print('dump audio...')
+        output = f' -vn -acodec copy {output}'
+    elif args.dump_meta:
+        print('dump meta...')
+        output = f' -f ffmetadata {output}'
+    elif args.replace_audio:
+        print('dump replace audio...')
+        output = f' -map 0:v -map 1:a -c:v copy -shortest {output}'
+    else:
+        print('nothing to do')
+        exit()
+    cmd += output
+
+    print(f'========= dumpper run: {cmd}')
+    os.system(cmd)
+    print(f'========= dumpper done: {cmd}')
+
+
+if __name__ == '__main__':
+    main()
