e0001ecb3c11ee0fa3f8781e45881e655152c866
jason.chou <jason.chou@ddhl.com>
2022-03-31 16:31:10 +0800

    add release

---
+++ scripts/release.bat
@@ -0,0 +1,17 @@
+call conda activate base
+
+cd ..
+
+del /s /q dist
+del /s /q build
+
+call conda remove -y --name ffhelper-py39 --all
+
+call conda create -y -n ffhelper-py39 python=3.9
+call conda run -n ffhelper-py39 python -m pip wheel -w dist --verbose .
+
+call pip install twine
+call twine upload dist/ffhelper*.whl
+
+
+
---
+++ scripts/release.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rm -R ./build
+rm -Rf ./dist
+sudo docker rmi build-ffhelper
+mkdir -p ./dist
+sudo docker build -t build-ffhelper -f docker/Dockerfile .
+sudo docker create --name build_ffhelper build-ffhelper --entrypoint /
+sudo docker cp build_ffhelper:/app/dist ./
+sudo docker rm -f build_ffhelper
+
+python3 -m pip install --upgrade twine
+python3 -m twine upload ./dist/ffhelper*.whl
